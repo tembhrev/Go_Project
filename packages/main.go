85c3c41170c02d34defdd733de9f13734b4c84ee
tembhrev <sw.vikastembhre@gmail.com>
2024-04-17 20:05:28 +0530

    packages

---
+++ packages/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+	"packages/helpers"
+)
+
+func main() {
+	log.Println("Hello")
+
+	var myVar helpers.SomeType
+	myVar.TypeName = "Some name"
+	log.Println(myVar.TypeName)
+}
